65e415e2d6418751c53d80a750124a83e018c959
Stephanie Daugherty <sdaugherty@gmail.com>
2013-02-27 22:08:57 -0500

    Fix development scripts for new piecrust.

--- serve_devsite.sh
+++ serve_devsite.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # This serves a development version of the site for testing, assuming the PieCrust files have been downloaded into _piecrust
 
-_piecrust/bin/chef serve --root=documents
+_piecrust/bin/chef --root=documents serve
+
