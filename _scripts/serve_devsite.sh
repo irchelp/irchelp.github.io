6b4daf2f157241b3b1ed9e5a1886b2b1dd449467
Moritz Wilhelmy <mw@barfooze.de>
2017-12-08 20:24:59 +0100

    This repository may have contained traces of PieCrust
    so I took the liberty and removed them.

--- _scripts/serve_devsite.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# This serves a development version of the site for testing, assuming the PieCrust files have been downloaded into _piecrust
-
-echo "This is slightly broken right now." 
-_piecrust/bin/chef --root=documents serve
-
