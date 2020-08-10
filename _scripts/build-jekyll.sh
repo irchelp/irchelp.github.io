7abab98e848870e5242ba075a21bae263ac32bab
bw1 <bw1@aol.at>
2020-08-10 10:19:35 +0200

    travis: htmlproofer can break travis

--- _scripts/build-jekyll.sh
+++ _scripts/build-jekyll.sh
@@ -5,5 +5,4 @@ bundle exec jekyll build --strict_front_matter
 # for right now, we don't want to test external sites
 # when we get closer to relaunch, remove this so that
 # we do test them.
-bundle exec htmlproofer ./_site --disable-external || echo "WARNING: Please fix the errors reported by htmlproofer above"
-# temporarily ignore html-proofer errors`
+bundle exec htmlproofer ./_site --disable-external
