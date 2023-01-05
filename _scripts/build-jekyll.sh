6a69aba16b7943272b813f0acefc7f57ef524549
bw1 <bw1@aol.at>
2023-01-05 16:39:45 +0100

    update travis

--- _scripts/build-jekyll.sh
+++ _scripts/build-jekyll.sh
@@ -7,5 +7,8 @@ bundle exec jekyll build --strict_front_matter
 # for right now, we don't want to test external sites
 # when we get closer to relaunch, remove this so that
 # we do test them.
-bundle exec htmlproofer ./_site --disable-external
-
+bundle exec htmlproofer ./_site \
+	--disable-external=true \
+	--enforce-https=false \
+	--allow-missing-href=true \
+	--check-internal-hash=false
